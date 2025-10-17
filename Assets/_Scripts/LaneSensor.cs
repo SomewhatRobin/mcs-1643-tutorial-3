5b1ea1ce7c8dabda024c37e4821c35d289049421
SomewhatRobin <jrobinso3@ltu.edu>
2025-10-17 16:02:07 -0400

    Actually Finished
    Added a sound for the score lane, remembered to make a build.

--- Assets/_Scripts/LaneSensor.cs
+++ Assets/_Scripts/LaneSensor.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class LaneSensor : MonoBehaviour
 {
     public int points = 250;
+    private AudioSource audioSrc;
+
+    void Start()
+    {
+        audioSrc = GetComponentInChildren<AudioSource>();
+    }
+
+
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.CompareTag("PlayerBall"))
         {
+            audioSrc.Play();
             ScoreManager.AddScore(points);
         }
     }
